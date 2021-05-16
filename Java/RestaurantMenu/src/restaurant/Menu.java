3c3dd6372b99a2b6300d902805d4c585ae0bfba9
Salini Arjunan <salini_arjunan@gmail.com>
2021-05-15 22:05:06 -0500

    first commit

---
+++ Java/RestaurantMenu/src/restaurant/Menu.java
@@ -0,0 +1,14 @@
+package restaurant;
+
+import java.util.ArrayList;
+
+public class Menu {
+    public static ArrayList<String> arrLstMenu = new ArrayList<>();
+
+
+
+    public static void main(String[] args) {
+        // write your code here
+        arrLstMenu.add("test");
+    }
+}
